356326a8027236fb0213c1e83276744584455105
Hans Hübner <hans.huebner@gmail.com>
2023-12-12 15:25:46 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -38478,13 +38478,13 @@ module.exports = {
     await downloadStatistics(
       owner,
       repo,
-      core.getInput('build_and_test.yml'),
-      core.getInput('artifact-name-regexp'),
+      core.getInput('build_and_test.yml', { required: true }),
+      core.getInput('artifact-name-regexp', { required: true }),
       tmpDir,
     )
-    const testFileRuntimeFile = core.getInput('test-file-runtime-file')
+    const testFileRuntimeFile = core.getInput('test-file-runtime-file', { required: true })
     await combineStatistics(tmpDir, testFileRuntimeFile)
-    await simpleGit
+    await simpleGit()
       .add(testFileRuntimeFile)
       .commit('updated test file runtime file')
   },
@@ -38492,11 +38492,11 @@ module.exports = {
   schedule: async () => {
     try {
       await schedule(
-        core.getInput('test-suites-file'),
-        core.getInput('runtime-info-repo'),
-        core.getInput('repo-root'),
-        core.getInput('output-prefix'),
-        parseInt(core.getInput('runner-count'), 10),
+        core.getInput('test-suites-file', { required: true }),
+        core.getInput('runtime-info-repo', { required: true }),
+        core.getInput('repo-root', { required: true }),
+        core.getInput('output-prefix', { required: true }),
+        parseInt(core.getInput('runner-count', { required: true }), 10),
       )
     } catch (e) {
       core.setFailed(e.message)
@@ -38506,10 +38506,10 @@ module.exports = {
   runner: async () => {
     try {
       await runner(
-        core.getInput('tests-to-run-file'),
-        core.getInput('failed-test-files-file'),
-        core.getInput('test-file-runtime-file'),
-        core.getInput('setup-venv'),
+        core.getInput('tests-to-run-file', { required: true }),
+        core.getInput('failed-test-files-file', { required: true }),
+        core.getInput('test-file-runtime-file', { required: true }),
+        core.getInput('setup-venv', { required: true }),
       )
     } catch (e) {
       core.setFailed(e.message)
